b4ba85c7bbd6d62b9601e1055aa09eb5c0da2b3b
bernouze <bernert.justin@gmail.com>
2015-02-05 02:13:54 +0100

    Revert "Revert "test vite fait de la map + movement cam""
    This reverts commit 0115e26d6fb687bf787ab0d8f5fcb52d70d17ee2.

---
+++ CubatorV3/core/src/com/mobile/TweenAccessors/Value.java
@@ -0,0 +1,17 @@
+package com.mobile.TweenAccessors;
+
+/**
+ * Created by justin on 2/4/2015.
+ */
+public class Value {
+    private float val = 1;
+
+    public float getValue() {
+        return val;
+    }
+
+    public void setValue(float newVal) {
+        val = newVal;
+    }
+
+}
--- CubatorV3/desktop/src/com/mobile/cubator/desktop/DesktopLauncher.java
+++ CubatorV3/desktop/src/com/mobile/cubator/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Cubator";
-        config.width = 272;
-        config.height = 408;
+        config.width = 1280;
+        config.height = 720;
 		new LwjglApplication(new CubatorGame(), config);
 	}
 }
